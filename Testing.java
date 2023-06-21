22b8e18b3d66516b46bf4b40b0ce70d2deb96c5c
Ted Hu <93688159+DragonTam3r1@users.noreply.github.com>
2023-06-20 21:18:21 -0700

    Add files via upload

---
+++ Testing.java
@@ -0,0 +1,13 @@
+//import java.util.Scanner;
+public class Testing {
+    public static void main(String[] args)
+    {
+        //Scanner input = new Scanner(System.in);
+        Patient testing = new Patient();
+        testing.printPatient();
+        //testing.editPatientInfo(input);
+        testing.newVitals();
+        testing.newVitals();
+        System.out.println("done!");
+    }
+}
